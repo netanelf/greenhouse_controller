4c4711e88715686b12e2c3c5cfc8d804ab1eb492
netanel <netanelf@gmail.com>
2015-10-06 23:42:16 +0300

    wrote sr_controller (shift register), need to be checked on hardware

--- core/controllers/relay_controller.py
+++ core/controllers/relay_controller.py
@@ -5,43 +5,43 @@ import logging
 
 class RelayController(object):
     def __init__(self, name, pin, state=0, simulate=True):
-        self._name = name
-        self._pin = pin
+        self.name = name
+        self.pin = pin
 
-        self._simulate = simulate
-        self._logger = logging.getLogger(name)
+        self.simulate = simulate
+        self.logger = logging.getLogger(name)
         if simulate is False:
             import RPi.GPIO as GPIO
             GPIO.setmode(GPIO.BOARD)
-            GPIO.setup(self._pin, GPIO.OUT)
+            GPIO.setup(self.pin, GPIO.OUT)
 
         self.change_state(new_state=state)
-        self._state = state
+        self.state = state
 
     def change_state(self, new_state):
-        if self._check_state(new_state):
-            if self._simulate:
-                self._state = new_state
+        if self.check_state(new_state):
+            if self.simulate:
+                self.state = new_state
             else:
                 import RPi.GPIO as GPIO
-                GPIO.output(self._pin, new_state)
+                GPIO.output(self.pin, new_state)
         else:
-            self._logger.info('state: {} is not legal'.format(new_state))
+            self.logger.info('state: {} is not legal'.format(new_state))
 
     def get_state(self):
-        return self._state
+        return self.state
 
     def get_name(self):
-        return self._name
+        return self.name
 
-    def _check_state(self, state):
+    def check_state(self, state):
         if state in [1, 0]:
             return True
         else:
             return False
 
     def __unicode__(self):
-        return 'name: {}, pin: {}, state: {}'.format(self._name, self._pin, self._state)
+        return 'name: {}, pin: {}, state: {}'.format(self.name, self.pin, self.state)
 
 
 if __name__ == '__main__':
---
+++ core/controllers/sr_controller.py
@@ -0,0 +1,90 @@
+__author__ = 'netanel'
+import time
+
+class SRController(object):
+    """
+    control a SN74HC595 Shift register
+    OE - output enable, not used hold at GROUND
+    SRCLR - clear shifty register, not used, hold at VCC
+    SRCLK - clock the shift register (input = SER), on LOW to HIGH transition
+    RCLK - load all registers into buffer, on LOW to HIGH transition
+    SER - data that will go into first register
+    """
+
+    def __init__(self, SER, RCLK, SRCLK, register_size=8, simulate=True):
+        self.ser = SER
+        self.rclk = RCLK
+        self.srclk = SRCLK
+        self.size = register_size
+        self.state = [1] * self.size
+        self.simulate = simulate
+
+        if not simulate:
+            import RPi.GPIO as GPIO
+            GPIO.setmode(GPIO.BOARD)
+            GPIO.setup(self.ser, GPIO.OUT)
+            GPIO.setup(self.rclk, GPIO.OUT)
+            GPIO.setup(self.srclk, GPIO.OUT)
+
+            GPIO.output(self.ser, 0)
+            GPIO.output(self.ser, 0)
+            GPIO.output(self.ser, 0)
+
+        self.clear_register()
+
+    def clear_register(self):
+        """
+        write 0 to all registers
+        :return:
+        """
+        for i in range(self.size):
+            self.shift_data(0)
+
+        self.load_output()
+        self.state = [0] * self.size
+
+    def shift_data(self, data):
+        """
+        shift data into register, data should be 0, 1
+        :param data:
+        :return:
+        """
+        if self.simulate:
+            return
+        else:
+            GPIO.output(self.ser, data)
+            GPIO.output(self.srclk, 1)
+            GPIO.output(self.srclk, 0)
+
+    def load_output(self):
+        """
+        load shift register data into output latches
+        :return:
+        """
+        if self.simulate:
+            return
+        else:
+            GPIO.output(self.rclk, 1)
+            GPIO.output(self.rclk, 0)
+
+    def change_bit(self, pin, new_state):
+        """
+        change one "bit" in the shift register, keep all other bits untouched
+        :param pin: pin/ bit number
+        :param new_state: new value
+        """
+        if not self.simulate:
+            new_full_state = list(self.state)
+            new_full_state[pin] = new_state
+            for i in new_full_state:
+                self.shift_data(i)
+            self.load_output()
+
+        self.state[pin] = new_state
+
+
+if __name__ == '__main__':
+    sr = SRController(SER=5, RCLK=6, SRCLK=7, simulate=False)
+    for i in range(8):
+        sr.change_bit(pin=i, new_state=1)
+        time.sleep(1)
