7f35c7c6922a0d191ba3c97a912171d4d45fd158
netanel <netanelf@gmail.com>
2015-10-16 23:13:17 +0300

    - added DS18B20 controller

---
+++ core/sensors/ds18b20_temp_controller.py
@@ -0,0 +1,39 @@
+__author__ = 'netanel'
+
+from sensor_controller import SensorController, Measurement
+from django.utils import timezone
+import random
+from core.drivers import ds18b20_driver
+
+
+class DS18B20TempController(SensorController):
+    """
+    DS18B20 temperature sensor controller
+    """
+    def __init__(self, name, device_id, simulate=True):
+        super(DS18B20TempController, self).__init__(name)
+        self._device_id = device_id
+        self._last_read = Measurement(sensor_name=self._name, time=timezone.now(), value=None)
+        self._simulate = simulate
+
+    def read(self):
+        super(DS18B20TempController, self).read()
+        if self._simulate:
+            t = self.simulate_data()
+        else:
+            t = ds18b20_driver.read_ds18b20_temp(sensor_id=self._device_id)
+            if t is None:
+                self._logger.error('could not read data from sensor: {},'.format(self._name))
+                t = 0
+        self._last_read = Measurement(sensor_name=self._name, time=timezone.now(), value=t)
+        return self._last_read
+
+    def simulate_data(self):
+        return random.randint(20, 50)
+
+if __name__ == '__main__':
+    from time import sleep
+    t_c = DS18B20TempController(name='demo_sensor', device_id='28-031467d282ff', simulate=False)
+    for i in range(10):
+        print t_c.read()
+        sleep(2)
