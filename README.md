e95e425b6da813d5a425b5b3d28cd77dcd9266cd
netanel <netanelf@gmail.com>
2015-12-05 15:11:40 +0200

    started creating readme for populations, added nice viewing of data from admin console

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # greenhouse_controller
+
+1. clone https://github.com/netanelf/greenhouse_controller.git
+2. clone https://github.com/adafruit/Adafruit-Raspberry-Pi-Python-Code.git
+3. install: 
+	python-django python-smbus i2c-tools build-essential python-dev python-openssl
+4. git clone https://github.com/adafruit/Adafruit_Python_DHT.git, 
+	cd Adafruit_Python_DHT
+	sudo python setup.py install
+
+
+web:
+1. delete db.sqlite3 (if exists)
+2. delete all migrations
+3. manage.py makemigrations greenhouse_app
+4. manage.py migrate
+5. python populate_greenhouse_app_rpi.py
+
--- web/greenhouse_django_project/greenhouse_app/admin.py
+++ web/greenhouse_django_project/greenhouse_app/admin.py
@@ -2,10 +2,25 @@ from django.contrib import admin
 from greenhouse_app.models import Sensor, SensorKind, Measure, Relay, TimeGovernor, Configurations
 # Register your models here.
 
+class RelayAdmin(admin.ModelAdmin):
+    list_display = ('name', 'pin', 'state', 'wanted_state', 'simulate', 'time_governor')
+
+
+class SensorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'kind', 'simulate', 'pin', 'i2c', 'device_id')
+
+
+class TimeGovernorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'kind', 'state')
+
+
+class ConfigurationsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'value', 'explanation')
+
 
 admin.site.register(SensorKind)
-admin.site.register(Sensor)
+admin.site.register(Sensor, SensorAdmin)
 admin.site.register(Measure)
-admin.site.register(Relay)
-admin.site.register(TimeGovernor)
-admin.site.register(Configurations)
+admin.site.register(Relay, RelayAdmin)
+admin.site.register(TimeGovernor, TimeGovernorAdmin)
+admin.site.register(Configurations, ConfigurationsAdmin)
