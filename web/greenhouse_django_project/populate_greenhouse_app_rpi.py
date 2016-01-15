ce3e1aa4d076291a9727e0a1aeb05cadbfe3c630
netanel <netanelf@gmail.com>
2016-01-15 14:34:16 +0200

    added a .gotignore file to ignore pyc and log files

--- web/greenhouse_django_project/populate_greenhouse_app_rpi.py
+++ web/greenhouse_django_project/populate_greenhouse_app_rpi.py
@@ -54,28 +54,28 @@ def populate_relays():
     t = TimeGovernor.objects.get_or_create(name='light1', kind='R', on_start_time='08:00:00', on_end_time='08:00:00', recurring_on_start_time='08:00:00', recurring_on_period=60, recurring_off_period=30)[0]
     t2 = TimeGovernor.objects.get_or_create(name='rec_90_90', kind='R', on_start_time='08:00:00', on_end_time='08:00:00', recurring_on_start_time='08:00:00', recurring_on_period=90, recurring_off_period=90)[0]
     
-    print 'creating relay: (name=light1, pin=0, state=1, wanted_state=1)'
+    print 'creating relay: (name=light1, pin=11, state=1, wanted_state=1)'
     r = Relay.objects.get_or_create(name='light1')[0]
-    r.pin = 0
+    r.pin = 11
     r.state = 1
     r.wanted_state = 1
     r.time_governor = t
     r.save()
 
-    print 'creating relay: (name=light2, pin=1, state=1, wanted_state=1)'
+    print 'creating relay: (name=light2, pin=13, state=1, wanted_state=1)'
     r = Relay.objects.get_or_create(name='light2')[0]
-    r.pin = 1
+    r.pin = 13
     r.state = 1
     r.wanted_state = 1
     r.save()
 
-    print 'creating relay: (name=fan, pin=2, state=1, wanted_state=1)'
+    print 'creating relay: (name=fan, pin=15, state=1, wanted_state=1)'
     r = Relay.objects.get_or_create(name='fan')[0]
-    r.pin = 2
+    r.pin = 15
     r.state = 1
     r.wanted_state = 1
     r.save()
-
+    '''
     print 'creating relay: (name=humidity, pin=3, state=1, wanted_state=1)'
     r = Relay.objects.get_or_create(name='humidity')[0]
     r.pin = 3
@@ -112,6 +112,7 @@ def populate_relays():
     r.wanted_state = 1
     r.time_governor = t2
     r.save()
+    '''
     
     
 def populate_configurations():
