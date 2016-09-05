541445f05cc5925214fdb03554c377c2ffc76c88
netanel <netanelf@gmail.com>
2016-09-05 23:17:06 +0300

    minor updates for real setup

--- run_full_linux.sh
+++ run_full_linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "arg1: " $1
-x-terminal-emulator -e sh run_web_linux.sh
-sudo sh run_brain_linux.sh $1
+x-terminal-emulator -e sh run_web_linux.sh &
+x-terminal-emulator -e sudo sh run_brain_linux.sh $1 &
--- web/greenhouse_django_project/populate_greenhouse_app_rpi.py
+++ web/greenhouse_django_project/populate_greenhouse_app_rpi.py
@@ -17,103 +17,59 @@ def populate_sensors():
     ds18b20 = SensorKind.objects.get_or_create(kind='ds18b20')[0]
     tsl2561 = SensorKind.objects.get_or_create(kind='tsl2561')[0]
 
-    print 'creating sensor: {}'.format('dht_1_temp')
-    s = Sensor.objects.get_or_create(name='dht_1_temp')[0]
+    print 'creating sensor: {}'.format('dht_temp')
+    s = Sensor.objects.get_or_create(name='dht_temp')[0]
     s.kind = dht_22_temp
     s.simulate = False
     s.pin = 37
     s.i2c = False
     s.save()
 
-    print 'creating sensor: {}'.format('dht_1_humidity')
-    s = Sensor.objects.get_or_create(name='dht_1_humidity')[0]
+    print 'creating sensor: {}'.format('dht_humidity')
+    s = Sensor.objects.get_or_create(name='dht_humidity')[0]
     s.kind = dht_22_humidity
     s.simulate = False
     s.pin = 37
     s.i2c = False
     s.save()
 
-    #print 'creating sensor: {}'.format('dht_2_temp')
-    #Sensor.objects.get_or_create(name='dht_2_temp', kind=dht_22_temp, simulate=False, pin=38, i2c=False)[0]
-    #print 'creating sensor: {}'.format('dht_2_humidity')
-    #Sensor.objects.get_or_create(name='dht_2_humidity', kind=dht_22_humidity, simulate=False, pin=38, i2c=False)[0]
 
-    print 'creating sensor: {}'.format('DS18B20_water')
-    Sensor.objects.get_or_create(name='DS18B20_water', kind=ds18b20, simulate=False, pin=99, i2c=False, device_id='28-011581dabaff')[0]
+    print 'creating sensor: {}'.format('DS18B20_temp')
+    Sensor.objects.get_or_create(name='DS18B20_temp', kind=ds18b20, simulate=False, pin=99, i2c=False, device_id='28-011581dabaff')[0]
 
-    #print 'creating sensor: {}'.format('DS18B20_indoor')
-    #Sensor.objects.get_or_create(name='DS18B20_indoor', kind=ds18b20, simulate=False, pin=99, i2c=False, device_id='28-031467eefbff')[0]
-
-    print 'creating sensor: {}'.format('TSL2561_lux_1')
-    Sensor.objects.get_or_create(name='lux_1', kind=tsl2561, simulate=False, pin=99, i2c=True, device_id='0x39')[0]
+    print 'creating sensor: {}'.format('TSL2561_lux')
+    Sensor.objects.get_or_create(name='lux', kind=tsl2561, simulate=False, pin=99, i2c=True, device_id='0x39')[0]
 
 
 def populate_relays():
 
-    t = TimeGovernor.objects.get_or_create(name='light1', kind='R', on_start_time='08:00:00', on_end_time='08:00:00', recurring_on_start_time='08:00:00', recurring_on_period=60, recurring_off_period=30)[0]
-    t2 = TimeGovernor.objects.get_or_create(name='rec_90_90', kind='R', on_start_time='08:00:00', on_end_time='08:00:00', recurring_on_start_time='08:00:00', recurring_on_period=90, recurring_off_period=90)[0]
+    t = TimeGovernor.objects.get_or_create(name='light_governer', kind='O', on_start_time='19:00:00', on_end_time='06:00:00', recurring_on_start_time='08:00:00', recurring_on_period=60, recurring_off_period=30)[0]
+    t2 = TimeGovernor.objects.get_or_create(name='fan_governer', kind='R', on_start_time='19:00:00', on_end_time='06:00:00', recurring_on_start_time='08:00:00', recurring_on_period=60*5, recurring_off_period=30)[0]
     
-    print 'creating relay: (name=light1, pin=1, state=1, wanted_state=1)'
-    r = Relay.objects.get_or_create(name='light1')[0]
+    print 'creating relay: (name=light, pin=1, state=1, wanted_state=1)'
+    r = Relay.objects.get_or_create(name='light')[0]
     r.pin = 0
     r.state = 1
     r.wanted_state = 1
     r.time_governor = t
     r.save()
 
-    print 'creating relay: (name=light2, pin=2, state=1, wanted_state=1)'
-    r = Relay.objects.get_or_create(name='light2')[0]
+    print 'creating relay: (name=fan, pin=2, state=1, wanted_state=1)'
+    r = Relay.objects.get_or_create(name='fan')[0]
     r.pin = 1
     r.state = 1
     r.wanted_state = 1
+    r.time_governor = t2
     r.save()
 
-    print 'creating relay: (name=fan, pin=3, state=1, wanted_state=1)'
-    r = Relay.objects.get_or_create(name='fan')[0]
+    print 'creating relay: (name=pump, pin=3, state=1, wanted_state=1)'
+    r = Relay.objects.get_or_create(name='pump')[0]
     r.pin = 2
-    r.state = 1
-    r.wanted_state = 1
-    r.save()
-    '''
-    print 'creating relay: (name=humidity, pin=3, state=1, wanted_state=1)'
-    r = Relay.objects.get_or_create(name='humidity')[0]
-    r.pin = 3
-    r.state = 1
-    r.wanted_state = 1
+    r.state = 0
+    r.wanted_state = 0
     r.save()
 
-    print 'creating relay: (name=humidity, pin=4, state=1, wanted_state=1)'
-    r = Relay.objects.get_or_create(name='fan2')[0]
-    r.pin = 4
-    r.state = 1
-    r.wanted_state = 1
-    r.save()
-    
-    print 'creating relay: (name=misc0, pin=5, state=1, wanted_state=1)'
-    r = Relay.objects.get_or_create(name='misc0')[0]
-    r.pin = 5
-    r.state = 1
-    r.wanted_state = 1
-    r.save()
-    
-    print 'creating relay: (name=misc1, pin=6, state=1, wanted_state=1)'
-    r = Relay.objects.get_or_create(name='misc1')[0]
-    r.pin = 6
-    r.state = 1
-    r.wanted_state = 1
-    r.time_governor = t2
-    r.save()
-        
-    print 'creating relay: (name=misc2, pin=7, state=1, wanted_state=1)'
-    r = Relay.objects.get_or_create(name='misc2')[0]
-    r.pin = 7
-    r.state = 1
-    r.wanted_state = 1
-    r.time_governor = t2
-    r.save()
-    '''
-    
-    
+
 def populate_configurations():
     c = Configurations.objects.get_or_create(name='manual_mode')[0]
     c.value=0
