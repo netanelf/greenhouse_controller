9043e60075218500a38d7ceeba4391b1fa991234
netanel <netanelf@gmail.com>
2016-09-05 23:26:31 +0300

    minor updates for real setup 2

--- run_web_linux.sh
+++ run_web_linux.sh
@@ -5,4 +5,4 @@ export PYTHONPATH=$PYTHONPATH:../greenhouse_controller:web/greenhouse_django_pro
 echo $PYTHONPATH
 echo "running web"
 cd web/greenhouse_django_project
-python manage.py runserver 192.168.0.105:8000
+python manage.py runserver 192.168.0.100:8000
