7e2386df9237fd5c57bc9f2551e3e215a4e07342
netanel <netanelf@gmail.com>
2017-03-15 18:57:05 +0200

    ip fix

--- run_web_linux.sh
+++ run_web_linux.sh
@@ -5,4 +5,4 @@ export PYTHONPATH=$PYTHONPATH:../greenhouse_controller:web/greenhouse_django_pro
 echo $PYTHONPATH
 echo "running web"
 cd web/greenhouse_django_project
-python manage.py runserver 10.0.0.2:8000
+python manage.py runserver 10.0.0.3:8000
